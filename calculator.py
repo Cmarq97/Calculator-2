bc49866d6e738ee92ffb46a510e9107d7f2601c1
Hackbright Student <no-reply@hackbrightacademy.com>
2018-03-07 18:05:19 -0800

    Final with invalid input flags

--- calculator.py
+++ calculator.py
@@ -9,27 +9,36 @@ from arithmetic import *
 
 while True:
     userinput = raw_input().split(" ")
-
-    if userinput[0] == "q":
-        break
-    else:
-        num1 = float(userinput[1])
-        if userinput[0] == 'square':
-            print square(num1)
-        elif userinput[0] == 'cube':
-            print cube(num1)
+    try:
+        if userinput[0] == "q":
+            break
         else:
-            num2 = float(userinput[2])
+            try:
+                num1 = float(userinput[1])
+                if userinput[0] == 'square':
+                    print square(num1)
+                elif userinput[0] == 'cube':
+                    print cube(num1)
+                else:
+                    try:
+                        num2 = float(userinput[2])
+                        if userinput[0] == "+":
+                            print add(num1, num2)
+                        elif userinput[0] == '-':
+                            print subtract(num1, num2)
+                        elif userinput[0] == '*':
+                            print multiply(num1, num2)
+                        elif userinput[0] == '/':
+                            print divide(num1, num2)
+                        elif userinput[0] == 'pow':
+                            print power(num1, num2)
+                        elif userinput[0] == 'mod':
+                            print mod(num1, num2)
+                    except ValueError:
+                        print "Your third input was invalid. Please try again"
 
-            if userinput[0] == "+":
-                print add(num1, num2)
-            elif userinput[0] == '-':
-                print subtract(num1, num2)
-            elif userinput[0] == '*':
-                print multiply(num1, num2)
-            elif userinput[0] == '/':
-                print divide(num1, num2)
-            elif userinput[0] == 'pow':
-                print power(num1, num2)
-            elif userinput[0] == 'mod':
-                print mod(num1, num2)
+            except ValueError:
+                print "Your second input was invalid. Please try again"
+
+    except IndexError or ValueError:
+        print "Your first input was invalid. Please try again"
